ef607eb1458151373fdcb2a6ed9dfcbedc0faaeb
Pranav Thakare <pranavthakare100404@gmail.com>
2026-05-27 17:19:20 +0530

    modified code

---
+++ parcel/app.js
@@ -0,0 +1,31 @@
+// import react from "react";
+// import reactDom from "react-dom/client";
+
+
+
+// const h1 = <h1 id="heading">Hello, Everyone!!</h1>
+
+
+// const Title = () => {
+//     return <h1>Functions</h1>
+// };
+
+// const heading = () =>{
+//     return(
+//         <div>
+//             <h1>Hello, World!</h1>
+//             {h1}
+//             <Title></Title>
+//             <Title />
+//             {Title()}
+//         </div>
+//     )
+// }
+
+
+
+
+
+
+// const root = reactDom.createRoot(document.getElementById("root"));
+// root.render(heading());
